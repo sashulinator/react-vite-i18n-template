08cb08474cf37ac106f72d6166c9a0d09af5a30e
Savchenko Alexander <sashulinator@gmail.com>
2022-09-03 17:43:18 +0400

    removeCompSchema api add to page

--- invalid-request-data.ts
+++ invalid-request-data.ts
@@ -1,10 +1,10 @@
 import { InterruptedError } from '@/utils/error/interrupted-error'
 
 export class InvalidRequestDataError extends InterruptedError {
-  response: unknown
+  data: unknown
 
   constructor(originError: Error, data: unknown) {
     super('Invalid Request Data', 'invalidRequestData', originError)
-    this.response = data
+    this.data = data
   }
 }
