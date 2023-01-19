9df18e3830c00c79dd6b24658289cbdf8a2a4811
savchenko alexander <asavchenko@datatech.ru>
2023-01-19 23:57:12 +0400

    refactoring[Error] Добавить assertions is

---
+++ index.ts
@@ -0,0 +1,7 @@
+export * from './assertions/error'
+export * from './is/causable'
+export * from './is/codable'
+export * from './is/error'
+export * from './types/causable'
+export * from './types/codable'
+
