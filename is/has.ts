351088011f99264648e866d0aa9d902a38ab7042
savchenko alexander <asavchenko@datatech.ru>
2023-01-20 17:53:41 +0400

    refactooring[utils/dictionary] Перенести has в is

--- is/has.ts
+++ is/has.ts
@@ -1,6 +1,6 @@
-import { AnyDictionary } from './types/any';
-import { Has } from './types/has';
-import { Key } from './types/key';
+import { AnyDictionary } from '../types/any';
+import { Has } from '../types/has';
+import { Key } from '../types/key';
 
 function has<K extends Key>(dictionary: unknown, key: K): dictionary is Has<K> {
   return (dictionary as AnyDictionary)?.hasOwnProperty(key)
