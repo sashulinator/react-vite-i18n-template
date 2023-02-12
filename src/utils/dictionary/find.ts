354f83c85b2f14972b684ec670a090e22f88e6b3
savchenko alexander <asavchenko@datatech.ru>
2023-02-12 13:36:10 +0400

    [utils/dictionary] refactoring "find"

--- src/utils/dictionary/find.ts
+++ src/utils/dictionary/find.ts
@@ -1,10 +1,12 @@
 import { Key } from '../core/types/key'
-import { Dictionary } from './types/dictionary'
 
-// TODO типы можно построже сделать
-export function find<T>(key: Key | undefined, object: undefined | Dictionary<T>): T | undefined {
-  if (key !== undefined) {
-    return object?.[key]
-  }
-  return undefined
+/**
+ * Why? Because TS force you to write `dictionary?.[key || '']` if they both undefined
+ */
+export function find<D extends Partial<Record<K, unknown>>, K extends Key>(
+  dictionary: undefined | D,
+  key: K | undefined
+): D[K] | undefined {
+  if (key === undefined) return undefined
+  return dictionary?.[key]
 }
