f58fb73b577c5b4448b990dffa6cf6333f36bc2f
savchenko alexander <asavchenko@datatech.ru>
2023-02-12 13:35:19 +0400

    [utils/dictionary] refactoring "add"

--- src/utils/dictionary/add.ts
+++ src/utils/dictionary/add.ts
@@ -1,9 +1,5 @@
 import { Key } from '../core/types/key'
-import { Dictionary } from './types/dictionary'
 
-// TODO полностью переработать типы
-export function add<T>(key: Key, value: T, object: Dictionary<T>): Dictionary<T> {
-  return { ...object, [key]: value }
+export function add<D extends Partial<Record<K, V>>, K extends Key, V>(dictionary: D, key: K, value: V): D {
+  return { ...dictionary, [key]: value }
 }
-
-export { add as replace }
